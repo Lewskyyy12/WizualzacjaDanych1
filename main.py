29f8a45677e5b945cc4bc6dc069449c38b0f0e0a
Lewskyyy12 <dawid.lewandowskyi@gmail.com>
2024-03-03 13:05:00 +0100

    Pierwszy commit

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
 print("Wizualizacja Dancyh")
+
+a=56
+print(a)
+print(type(a))
+
+b=5.5
+print(b)
+print(type(b))
+
+zmienna_tekst = "Zmienna tekst"
+print(zmienna_tekst)
+print(type(zmienna_tekst))
+
+a = 6
+b = 3
+
+c = a + b
+print(c)
+d = a - b
+print(d)
+e = 4
+f = b // a
+print(f)
+f = a // b
+print(f)
+g = a ** 2
+print(g)
+h = pow(a,2)
+print(h)
+i = 6 **(1/2)
+print(i)
+j=pow(6, 1/2)
+print(j)
+k = 'Wizualizacja Danych'
+l = 'grupa'
+m = 1
+print(k +" "+ l + " "+ str(m))
+print('Liczba a jest rowna {:.2f}, liczba b jest rowna {:d}'.format(a,b) )
+print('Liczba a jest rowna {}, liczba b jest rowna {}'.format(a,b) )
