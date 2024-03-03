d24282f344d5c39a802f77e3862b79ac98e508b3
Lewskyyy12 <dawid.lewandowskyi@gmail.com>
2024-03-03 13:31:50 +0100

    Pierwszy commit

--- main.py
+++ main.py
@@ -1,40 +1,94 @@
-print("Wizualizacja Dancyh")
+# import sys
+# print("Wizualizacja Dancyh")
+#
+# a=56
+# print(a)
+# print(type(a))
+#
+# b=5.5
+# print(b)
+# print(type(b))
+#
+# zmienna_tekst = "Zmienna tekst"
+# print(zmienna_tekst)
+# print(type(zmienna_tekst))
+#
+# a = 6.55
+# b = 3
+#
+# c = a + b
+# print(c)
+# d = a - b
+# print(d)
+# e = 4
+# f = b // a
+# print(f)
+# f = a // b
+# print(f)
+# g = a ** 2
+# print(g)
+# h = pow(a,2)
+# print(h)
+# i = 6 **(1/2)
+# print(i)
+# j=pow(6, 1/2)
+# print(j)
+# k = 'Wizualizacja Danych'
+# l = 'grupa'
+# m = 1
+# print(k +" "+ l + " "+ str(m))
+# print('Liczba a jest rowna {:.2f}, liczba b jest rowna {:.3f}'.format(a,b) )
+# print('Liczba a jest rowna {}, liczba b jest rowna {}'.format(a,b) )
+#
+# a=input("Wprowadz liczbe:")
+# print(a)
+# print(type(a))
+# a=int(a)
+# print(a*5)
+# print(type(a))
+#
+# sys.stdout.write('Wprowadz liczbe: ')
+# b=sys.stdin.readline()
+# print(b)
+# print(b + ' to warotsc liczby b')
+# print(type(b))
 
-a=56
-print(a)
-print(type(a))
+# print('Listy')
+#
+# lista = [5, 6.6 ,34, 'a', 'b', [2,3,4], 'ab']
+# print(lista)
+# lista.append(67)
+# print(lista)
+# lista.insert(2,'c')
+# print(lista)
+# lista.extend([20,21,22])
+# print(lista)
+# lista.pop()
+# print(lista)
+# lista.pop(2)
+# print(lista)
+# lista.remove([2,3,4])
+# print(lista)
+# del lista[1]
+# print(lista)
+# # del lista
+# print(lista)
+# lista.reverse()
+# print(lista)
+# #lista.sort()
+# #print(lista)
 
-b=5.5
-print(b)
-print(type(b))
+print('Slonwik')
 
-zmienna_tekst = "Zmienna tekst"
-print(zmienna_tekst)
-print(type(zmienna_tekst))
+slownik = {'klucz': 'wartosc', 1:2, 'a': 5, 4: 'b'}
+print(slownik)
+print(slownik[4])
+slownik[6]= 45
+print(slownik)
+slownik.pop(1)
+print(slownik)
+print(slownik.keys())
+print(slownik.values())
+del slownik[6]
+print(slownik)
 
-a = 6
-b = 3
-
-c = a + b
-print(c)
-d = a - b
-print(d)
-e = 4
-f = b // a
-print(f)
-f = a // b
-print(f)
-g = a ** 2
-print(g)
-h = pow(a,2)
-print(h)
-i = 6 **(1/2)
-print(i)
-j=pow(6, 1/2)
-print(j)
-k = 'Wizualizacja Danych'
-l = 'grupa'
-m = 1
-print(k +" "+ l + " "+ str(m))
-print('Liczba a jest rowna {:.2f}, liczba b jest rowna {:d}'.format(a,b) )
-print('Liczba a jest rowna {}, liczba b jest rowna {}'.format(a,b) )
